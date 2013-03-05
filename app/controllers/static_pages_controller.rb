09175c572939abd87fc2c25ad9c4619792158df6
David Johnson <david@davidjohnson.me>
2013-03-05 14:18:21 -0600

    modelling users

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,5 +1,11 @@
 class StaticPagesController < ApplicationController
   def home
+  	@micropost = current_user.microposts.build if signed_in?
+    if current_user
+    	@feed_items = current_user.feed.paginate(page: params[:page])
+    else
+      @feed_items = []
+    end
   end
 
   def help
